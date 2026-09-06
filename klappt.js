9d4b169d56e8dfbde54988bec8a31d3f1933e82e
Viktor Filinkov <x6prlie@gmail.com>
2026-09-06 00:05:58 +0000

    upd

--- klappt.js
+++ klappt.js
@@ -48,7 +48,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmphkostk02.js
+// include: /tmp/tmpz59w8ntu.js
 if (!Module["expectedDataFileDownloads"]) Module["expectedDataFileDownloads"] = 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -195,7 +195,7 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: /tmp/tmphkostk02.js
+// end include: /tmp/tmpz59w8ntu.js
 // include: /home/x/src/klappt/src/platform/web/web_persist.js
 Module.preRun = Module.preRun || [];
 
